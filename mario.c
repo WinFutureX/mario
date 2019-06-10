1e6cd37838006658fbe77ce67dfdd67236a079b3
Kelsey Boey <kelseyboey@hotmail.com>
2019-06-10 23:12:55 +0800

    now functionally more in line with the original

--- mario.c
+++ mario.c
@@ -1,36 +1,33 @@
 // mario.c: print out a half-pyramid between 1-23, as seen at the end of every level in SMB
+//     ##
+//    ###
+//   ####
+//  #####
+// ######
 #include <stdio.h>
 int main()
 {
 	int height = 0;
-	char term;			// trick to check enter key
-	printf ("Height: ");
-	if (scanf ("%d%c", &height, &term) != 2 || term != '\n' || height < 0 || height > 23)
+	int result;
+	char st[1024];
+	do
 	{
-		printf ("Invalid value.\n");
-		printf ("Valid values are between 1 and 23.\n");
-		return 1;
+		printf("Height: ");
+		fgets(st, sizeof(st), stdin);
+		result = sscanf(st, "%d", &height);
 	}
-	else
+	while (height < 1 || height > 23);
+	for(int line = 0; line < height; line++)
 	{
-		#ifndef _WIN32
-		printf("\n");
-		#endif
-		for (int line = 0; line < height; line++)
+		for(int spaces = height - line; spaces > 1; spaces--)
 		{
-			for (int spaces = height - line; spaces > 1; spaces--)
-			{
-				printf (" ");
-			}
-			for (int hashes = 0; hashes < line + 2; hashes++)
-			{
-				printf ("#");
-			}
-		printf ("\n");
+			printf(" ");
+		}
+		for(int hashes = 0; hashes < line + 2; hashes++)
+		{
+			printf("#");
+		}
+		printf("\n");
 	}
-	#ifndef _WIN32
-	printf("\n");
-	#endif
 	return 0;
-	}
 }
